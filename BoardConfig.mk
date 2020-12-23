741677a4477f5aebb1aab6d6d833322431fa4f51
TheSync <repo-sync@outlook.com>
2020-12-23 20:38:49 +0700

    onc: cleanup
    Signed-off-by: TheSync <repo-sync@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -43,32 +43,16 @@ TARGET_BOARD_PLATFORM := msm8953
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno506
 
 # Kernel
-BOARD_KERNEL_CMDLINE := \
-    console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 \
-    androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 \
-    lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci \
-    earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image \
-    androidboot.usbconfigfs=true loop.max_part=7 \
-    androidboot.selinux=permissive
-
-BOARD_KERNEL_IMAGE_NAME      := Image.gz-dtb
-BOARD_KERNEL_BASE            := 0x80000000
-BOARD_KERNEL_PAGESIZE        := 2048
-BOARD_KERNEL_OFFSET          := 0x00008000
-BOARD_RAMDISK_OFFSET         := 0x01000000
-BOARD_SECOND_OFFSET          := 0x00f00000
-BOARD_KERNEL_TAGS_OFFSET     := 0x00000100
+BOARD_KERNEL_BASE := 0x80000000
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_serial_dm,0x78af000 firmware_class.path=/vendor/firmware_mnt/image androidboot.usbconfigfs=true loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_PAGESIZE := 2048
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
+BOARD_INCLUDE_RECOVERY_DTBO := true
+BOARD_PREBUILT_DTBOIMAGE := $(LOCAL_PATH)/prebuilt/dtbo.img
 BOARD_BOOTIMG_HEADER_VERSION := 1
-
-BOARD_INCLUDE_RECOVERY_DTBO  := true
-
-TARGET_PREBUILT_KERNEL       := $(LOCAL_PATH)/prebuilt/Image.gz-dtb
-BOARD_PREBUILT_DTBOIMAGE     := $(LOCAL_PATH)/prebuilt/dtbo.img
-
-BOARD_MKBOOTIMG_ARGS := \
-    --ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
-    --tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
-    --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)    
 
 # Assert
 TARGET_OTA_ASSERT_DEVICE := onc,onclite
@@ -89,7 +73,6 @@ BOARD_VENDORIMAGE_PARTITION_SIZE := 1073741824
 
 # System as root
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
-BOARD_SUPPRESS_SECURE_ERASE := true
 
 # File systems
 TARGET_USERIMAGES_USE_EXT4 := true
@@ -112,11 +95,12 @@ TW_THEME := portrait_hdpi
 BOARD_HAS_LARGE_FILESYSTEM := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 RECOVERY_SDCARD_ON_DATA := true
+BOARD_HAS_NO_REAL_SDCARD := true
 TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 255
-TW_DEFAULT_BRIGHTNESS := 102
+TW_MAX_BRIGHTNESS := 4095
+TW_DEFAULT_BRIGHTNESS := 50
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_EXTRA_LANGUAGES := true
 TW_DEFAULT_LANGUAGE := en
